8b2dbe7121f4b0611046a5257ccec6b538c98827
mrange <marten_range@hotmail.com>
2016-12-29 22:57:43 +0100

    Added perflazy

---
+++ src/perflazy/README.md
@@ -0,0 +1,96 @@
+# On the cost of being lazy
+
+Previously I have looked at performance of different [persistent hash maps](https://gist.github.com/mrange/d6e7415113ebfa52ccb660f4ce534dd4) and [data pipelines](https://gist.github.com/mrange/09ba6d7541468de49426eeb81a8b660c) in .NET. Now I wonder:
+
+*What is the cost of being lazy?*
+
+In F# we can define a lazy value like this:
+
+```fsharp
+let x = lazy expensiveCalculation ()  // Delay the expensive computation
+let y = x.Value                       // Forces the computation and returns the cached result
+let z = x.Value                       // Returns the cached result
+```
+
+What is the CPU cost of this delayed computation and how does `lazy` compare with alternatives?
+
+To test it I have created a simple algorithm that allows me to control the ratio of how often the lazy value will be be recreated so that we can see how the relative cost of being lazy depends on the ratio. The delayed computation is very cheap (returns an integer) as we like to measure the cost of being lazy.
+
+```fsharp
+let inline delay i              = lazy i
+let inline value (l : Lazy<_>)  = l.Value
+
+
+let createTestCases count ratio =
+  let rec simpleLoop l r s i =
+    if i < count then
+      let r = r + ratio
+      if r >= 1. then
+        let r = r - 1.
+        let l = delay i // Reset the lazy value to force recomputation
+        simpleLoop l r (s + value l) (i + 1)
+      else
+        simpleLoop l r (s + value l) (i + 1) // Use the cached value
+    else
+      s
+
+  let simple () =
+    simpleLoop (delay 0) 0. 0 0
+
+  simple
+```
+
+I vary the ratio from `0%` (only uses the cached value) to `100%` (performs computation every time) in step of `20%`.
+
+These are the alternatives I selected:
+
+0. **no lazy**                         - The control, the computation is done every time. As the computation is extremely cheap we expect this to be the fastest and we will just the control to compare the cost of being lazy.
+0. **lazy**                            - Uses `F#` lazy
+0. **Lazy (Execution & Publication)**  - `F#` internally uses .NET `Lazy<'T>` with Execution & Publication protection so we expect the performance to be identical.
+0. **Lazy (Publication)**              - Same as above but here we only uses publication protection. That means that the computation might be carried out by several threads but only one of the results are cached. There subtleties that I haven't dug into yet on whether all threads will see the same instance or not?
+0. **Lazy (None)**                     - No thread safety, if multiple threads are executing behavior is undefined. Doesn't cache exceptions.
+0. **Flag (Trivial)**                  - A lazy implementation using a simple unprotected boolean to decide whether a value is computed or not. Doesn't cache exceptions.
+0. **Flag (Compact)**                  - A lazy implementation letting the value act both value and flag. Doesn't cache exceptions.
+0. **Flag (Protected)**                - A lazy implementation that uses a simple publication protection scheme. Unfortunately so simple that different threads might see different instances. Doesn't cache exceptions.
+0. **Flag (Full protection)**          - A lazy implementation that uses `Monitor` to achieve Execution & Publication protection. Doesn't cache exceptions.
+0. **Flag (Full protection w. DC)**    - A lazy implementation that uses `Monitor` to achieve Execution & Publication protection and Double Check pattern to reduce cost of reading a cached value. Doesn't cache exceptions.
+
+## Performance in Milliseconds - F# 4, .NET 4.6.2, x64
+
+![Performance in Milliseconds - F# 4, .NET 4.6.2, x64](http://i.imgur.com/QSue3uT.png)
+
+As expected **no lazy** is the fastest overall (because the computation is very cheap).
+
+At `0%` all lazy implementations except **Flag (Compact)** and **Flag (Full protection)** has similar performance. **Flag (Compact)** does a type check each time it is dereferenced, this is the cost we see. **Flag (Full protection)** does a `Monitor.Enter`/`Monitor.Leave` when it's dereferenced which is costly compared to the other alternatives.
+
+For all lazy implementations except **Flag (Full protection)** the overhead seems to be roughly linear to the ratio.
+
+At `100%` we see that **lazy** incurs a 50x performance overhead. It is also somewhat surprising that **lazy** does worse than **Lazy (Execution & Publication)** as `F#` uses the same implementation but it turns out that `FSharp.Core` allocates an extra function object when we use `lazy`.
+
+Because of the forgiving memory model of `i86` it turns out that the cost of **Flag (Trivial)** and **Flag (Protected)** is almost identical. This might not be true on `ARM` or `PowerPC`.
+
+In all these cases we have no contention of the locks so the performance will look different in a concurrent environment with heavy contention. What is the cost of locks under contention? That is a topic for another blog post.
+
+## Collection Count in Milliseconds - F# 4, .NET 4.6.2, x64
+
+![Collection Count in Milliseconds - F# 4, .NET 4.6.2, x64](http://i.imgur.com/KIDxuyZ.png)
+
+Collection count gives an indication on the number of objects allocated by the lazy implementations. Lower is better.
+
+For all lazy implementations the overhead seems to be roughly linear to the ratio.
+
+All flag implementations except **Flag Compact** incurs the same overhead where **Flag Compact** ironically has a higher overhead. This is because **Flag Compact** needs to box the integer value.
+
+## Reflections
+
+Is it a problem that for `lazy` at `100%` the overhead is large? Isn't it so that in most realistic cases the ratio will be closer to `0%` than `100%`. In at least one case the opposite is true, consider [`Seq.upto`](https://github.com/Microsoft/visualfsharp/blob/master/src/fsharp/FSharp.Core/seq.fs#L274). It turns out `Seq.upto` uses `Lazy<_>` to cache the current value (or exception). Normally the current value is accessed just once and then the next value is computed. That means in this case the ratio is closer to `100%` and then incurs a large overhead if the computation is cheap. During my comparison of data pipelines in .NET this turned out to be the source of most of the overhead of `Seq` over other pipelines.
+
+Personally, I am sceptical of mutally exclusive regions of code (like `Monitor.Enter`/`Monitor.Exit`). One of my reasons for this is the dreaded priority inversion. Now on `Windows` and non real-time `Linux` this isn't much of a problem in general but when working with real-time system priority version is real problem. In addition, the real-time distributions of `Linux` I had the pleasure of working with had severe problems in the futex implementation which meant that a futex could lock-out a different process (!) if we are unlucky. So by avoiding mutually exclusive regions we avoid these problems.
+
+On the other hand, implementing thread-safe code without mutually exclusive regions is really hard as it requires whole different understanding of how the compiler, jitter CPU, cache and memory system rewrites your code in order to speed it up. Basically, reads are scheduled earlier and writes are delayed. Reads & writes that are deemed unnecessary may be eliminated. These rewrites works brilliantly in a **non-concurrent** environment. In a **concurrent** the rewrites makes it impossible for you to look at the code and deduce what happens (because this is not the program that is being executed). Because of this the simple [double-check locking](https://en.wikipedia.org/wiki/Double-checked_locking) is actually quite hard to implement. Mutually exclusive regions restores sanity but as mentioned above has their own problem.
+
+Finally, know that being lazy has costs. If the computation is cheap it might be better to do the computation repeatedly instead of caching the result.
+
+Hope this was interesting to you,
+
+Mårten
---
+++ src/perflazy/perflazy/Program.fs
@@ -0,0 +1,456 @@
+﻿module PerformanceTests =
+  open FSharp.Core.Printf
+
+  open System
+  open System.Collections
+  open System.Diagnostics
+  open System.IO
+  open System.Text
+
+  // now () returns current time in milliseconds since start
+  let now : unit -> int64 =
+    let sw = Stopwatch ()
+    sw.Start ()
+    fun () -> sw.ElapsedMilliseconds
+
+  // time estimates the time 'action' repeated a number of times
+  let time repeat action =
+    let inline cc i       = System.GC.CollectionCount i
+
+    let v                 = action ()
+
+    System.GC.Collect (2, System.GCCollectionMode.Forced, true)
+
+    let bcc0, bcc1, bcc2  = cc 0, cc 1, cc 2
+    let b                 = now ()
+
+    for i in 1..repeat do
+      action () |> ignore
+
+    let e = now ()
+    let ecc0, ecc1, ecc2  = cc 0, cc 1, cc 2
+
+    v, (e - b), ecc0 - bcc0, ecc1 - bcc1, ecc2 - bcc2
+
+  let inline dbreak () = System.Diagnostics.Debugger.Break ()
+
+  type TestResult =  TestResult of string*string*string*int64*int*int*int
+  let testResult t y x tm cc0 cc1 cc2 = TestResult (t, y, x, tm, cc0, cc1, cc2)
+  let testClass (TestResult (t, _, _, _, _, _, _))  = t
+  let testY     (TestResult (_, y, _, _, _, _, _))  = y
+  let testX     (TestResult (_, _, x, _, _, _, _))  = x
+
+  module NoLazyPerf =
+    let createTestCases count ratio =
+      let rec simpleLoop v r s i =
+        if i < count then
+          let r = r + ratio
+          if r >= 1. then
+            let r = r - 1.
+            let v = i
+            simpleLoop v r (s + v) (i + 1)
+          else
+            simpleLoop v r (s + v) (i + 1)
+        else
+          s
+
+      let simple () = 
+        simpleLoop 0 0. 0 0
+
+      simple
+
+  module LazyPerf =
+    let inline delay i              = lazy i
+    let inline value (l : Lazy<_>)  = l.Value
+
+
+    let createTestCases count ratio =
+      let rec simpleLoop l r s i =
+        if i < count then
+          let r = r + ratio
+          if r >= 1. then
+            let r = r - 1.
+            let l = delay i
+            simpleLoop l r (s + value l) (i + 1)
+          else
+            simpleLoop l r (s + value l) (i + 1)
+        else
+          s
+
+      let simple () = 
+        simpleLoop (delay 0) 0. 0 0
+
+      simple
+
+  module TrivialFlagPerf =
+    type Lazy<'T>(f : unit -> 'T) =
+      let mutable hasValue  = false
+      let mutable value     = Unchecked.defaultof<'T>
+
+      member x.Value =
+        if hasValue then
+          value
+        else
+          value <- f ()
+          hasValue <- true
+          value
+
+    let inline delay i              = Lazy<_> (fun () -> i)
+    let inline value (l : Lazy<_>)  = l.Value
+
+
+    let createTestCases count ratio =
+      let rec simpleLoop l r s i =
+        if i < count then
+          let r = r + ratio
+          if r >= 1. then
+            let r = r - 1.
+            let l = delay i
+            simpleLoop l r (s + value l) (i + 1)
+          else
+            simpleLoop l r (s + value l) (i + 1)
+        else
+          s
+
+      let simple () = 
+        simpleLoop (delay 0) 0. 0 0
+
+      simple
+
+  module CompactFlagPerf =
+    type Lazy<'T>() =
+      let mutable value     = null : obj
+
+      member x.UnsafeDelay (f : unit -> 'T) =
+        value <- f :> obj
+
+      member x.Value =
+        match value with
+        | :? 'T as v  -> v
+        | _           ->
+          let f = value :?> unit -> 'T
+          let v = f ()
+          value <- box v
+          v
+
+    let inline delay i              = let l = Lazy<'T> () in l.UnsafeDelay (fun () -> i); l
+    let inline value (l : Lazy<_>)  = l.Value
+
+
+    let createTestCases count ratio =
+      let rec simpleLoop l r s i =
+        if i < count then
+          let r = r + ratio
+          if r >= 1. then
+            let r = r - 1.
+            let l = delay i
+            simpleLoop l r (s + value l) (i + 1)
+          else
+            simpleLoop l r (s + value l) (i + 1)
+        else
+          s
+
+      let simple () = 
+        simpleLoop (delay 0) 0. 0 0
+
+      simple
+
+  module ProtectedFlagPerf =
+    open System.Threading
+
+    type Lazy<'T>(f : unit -> 'T) =
+      let mutable hasValue  = false
+      let mutable value     = Unchecked.defaultof<'T>
+
+      member x.Value =
+        let v = value
+        if Volatile.Read &hasValue then
+          v
+        else
+          let v = f ()
+          value <- v
+          Volatile.Write (&hasValue, true)
+          v
+
+    let inline delay i              = Lazy<_> (fun () -> i)
+    let inline value (l : Lazy<_>)  = l.Value
+
+
+    let createTestCases count ratio =
+      let rec simpleLoop l r s i =
+        if i < count then
+          let r = r + ratio
+          if r >= 1. then
+            let r = r - 1.
+            let l = delay i
+            simpleLoop l r (s + value l) (i + 1)
+          else
+            simpleLoop l r (s + value l) (i + 1)
+        else
+          s
+
+      let simple () = 
+        simpleLoop (delay 0) 0. 0 0
+
+      simple
+
+  module FullProtectionFlagPerf =
+    open System.Threading
+
+    type Lazy<'T>(f : unit -> 'T) =
+      let mutable hasValue  = false
+      let mutable value     = Unchecked.defaultof<'T>
+
+      member x.Value =
+        Monitor.Enter x
+        try
+          if hasValue then
+            value
+          else
+            value <- f ()
+            hasValue <- true
+            value
+        finally
+          Monitor.Exit x
+
+    let inline delay i              = Lazy<_> (fun () -> i)
+    let inline value (l : Lazy<_>)  = l.Value
+
+
+    let createTestCases count ratio =
+      let rec simpleLoop l r s i =
+        if i < count then
+          let r = r + ratio
+          if r >= 1. then
+            let r = r - 1.
+            let l = delay i
+            simpleLoop l r (s + value l) (i + 1)
+          else
+            simpleLoop l r (s + value l) (i + 1)
+        else
+          s
+
+      let simple () = 
+        simpleLoop (delay 0) 0. 0 0
+
+      simple
+
+  module FullProtectionFlag2Perf =
+    open System.Threading
+
+    type Lazy<'T>(f : unit -> 'T) =
+      let mutable hasValue  = false
+      let mutable value     = Unchecked.defaultof<'T>
+
+      member x.Value =
+        let v = value
+        if Volatile.Read &hasValue then
+          value
+        else
+          Monitor.Enter x
+          try
+            if Volatile.Read &hasValue then
+              value
+            else
+              value <- f ()
+              Volatile.Write (&hasValue, true)
+              value
+          finally
+            Monitor.Exit x
+
+    let inline delay i              = Lazy<_> (fun () -> i)
+    let inline value (l : Lazy<_>)  = l.Value
+
+
+    let createTestCases count ratio =
+      let rec simpleLoop l r s i =
+        if i < count then
+          let r = r + ratio
+          if r >= 1. then
+            let r = r - 1.
+            let l = delay i
+            simpleLoop l r (s + value l) (i + 1)
+          else
+            simpleLoop l r (s + value l) (i + 1)
+        else
+          s
+
+      let simple () = 
+        simpleLoop (delay 0) 0. 0 0
+
+      simple
+
+  module LazyProtectedExecutionAndPublicationPerf =
+    open System.Threading
+
+    let inline delay i              = Lazy<_> (Func<_> (fun () -> i), LazyThreadSafetyMode.ExecutionAndPublication)
+    let inline value (l : Lazy<_>)  = l.Value
+
+    let createTestCases count ratio =
+      let rec simpleLoop l r s i =
+        if i < count then
+          let r = r + ratio
+          if r >= 1. then
+            let r = r - 1.
+            let l = delay i
+            simpleLoop l r (s + value l) (i + 1)
+          else
+            simpleLoop l r (s + value l) (i + 1)
+        else
+          s
+
+      let simple () = 
+        simpleLoop (delay 0) 0. 0 0
+
+      simple
+
+  module LazyProtectedPublicationPerf =
+    open System.Threading
+
+    let inline delay i              = Lazy<_> (Func<_> (fun () -> i), LazyThreadSafetyMode.PublicationOnly)
+    let inline value (l : Lazy<_>)  = l.Value
+
+
+    let createTestCases count ratio =
+      let rec simpleLoop l r s i =
+        if i < count then
+          let r = r + ratio
+          if r >= 1. then
+            let r = r - 1.
+            let l = delay i
+            simpleLoop l r (s + value l) (i + 1)
+          else
+            simpleLoop l r (s + value l) (i + 1)
+        else
+          s
+
+      let simple () = 
+        simpleLoop (delay 0) 0. 0 0
+
+      simple
+
+  module LazyNoProtectionPerf =
+    open System.Threading
+
+    let inline delay i              = Lazy<_> (Func<_> (fun () -> i), LazyThreadSafetyMode.None)
+    let inline value (l : Lazy<_>)  = l.Value
+
+
+    let createTestCases count ratio =
+      let rec simpleLoop l r s i =
+        if i < count then
+          let r = r + ratio
+          if r >= 1. then
+            let r = r - 1.
+            let l = delay i
+            simpleLoop l r (s + value l) (i + 1)
+          else
+            simpleLoop l r (s + value l) (i + 1)
+        else
+          s
+
+      let simple () = 
+        simpleLoop (delay 0) 0. 0 0
+
+      simple
+
+
+  let run () =
+#if DEBUG
+    let count   = 100
+#else
+    let count   = 10000000
+#endif
+    let ratios  =
+      [|
+#if DEBUG
+        0.33
+#else
+        0.0
+        0.2
+        0.4
+        0.6
+        0.8
+        1.0
+#endif
+      |]
+
+    let testCases =
+      [|
+        "no lazy"                         , NoLazyPerf.createTestCases
+        "lazy"                            , LazyPerf.createTestCases
+        "Lazy (Execution & Publication)"  , LazyProtectedExecutionAndPublicationPerf.createTestCases
+        "Lazy (Publication)"              , LazyProtectedPublicationPerf.createTestCases
+        "Lazy (None)"                     , LazyNoProtectionPerf.createTestCases
+        "Flag (Trivial)"                  , TrivialFlagPerf.createTestCases
+        "Flag (Compact)"                  , CompactFlagPerf.createTestCases
+        "Flag (Protected)"                , ProtectedFlagPerf.createTestCases
+        "Flag (Full protection)"          , FullProtectionFlagPerf.createTestCases
+        "Flag (Full protection w. DC)"    , FullProtectionFlag2Perf.createTestCases
+      |]
+    let results = ResizeArray 16
+
+    for ratio in ratios do
+
+      printfn "Running test cases with ratio=%f" ratio
+
+      let x = sprintf "%d%%" (ratio*100. |> round |> int)
+
+      for name, creator in testCases do
+        printfn "  Running test case %A" name
+
+        let result t a =
+          let v, time, cc0, cc1, cc2 = time 1 a
+          results.Add <| testResult t name x time cc0 cc1 cc2
+          printfn "    = %A" v
+
+        let simple = creator count ratio
+
+        result "Simple" simple
+
+    let results = results.ToArray ()
+
+    let testXs      = results |> Array.groupBy testX |> Array.map fst
+    let testYs      = results |> Array.groupBy testY
+    let testClasses = results |> Array.groupBy testClass
+
+    let header  = "Name" + (testXs |> Array.map (fun i -> ",'" + string i) |> Array.reduce (+))
+
+    for name, results in testClasses do
+      use perf      = new StreamWriter ("perf_" + name + ".csv")
+      use cc        = new StreamWriter ("cc_"   + name + ".csv")
+      let line sw l = (sw : StreamWriter).WriteLine (l : string)
+      let linef sw f= kprintf (line sw) f
+
+      line perf header
+      line cc   header
+
+      for name, result in testYs do
+        let write sb s  = (sb : StringBuilder).Append (s : string) |> ignore
+        let field sb s  = (sb : StringBuilder).Append ',' |> ignore; write sb s
+        let fieldf sb f = kprintf (field sb) f
+        let psb         = StringBuilder 16
+        let csb         = StringBuilder 16
+        write psb name
+        write csb name
+        let m = result |> Array.map (fun tc -> testX tc, tc) |> Map.ofArray
+        for testInner in testXs do
+          let (TestResult (_, _, _, tm, cc0 ,_, _)) = m.[testInner]
+          fieldf psb "%d" tm
+          fieldf csb "%d" cc0
+
+        line perf <| psb.ToString ()
+        line cc   <| csb.ToString ()
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+  try
+    Environment.CurrentDirectory <- AppDomain.CurrentDomain.BaseDirectory
+    PerformanceTests.run ()
+    0
+  with
+  | e ->
+    printfn "Caught: %s" e.Message
+    999
