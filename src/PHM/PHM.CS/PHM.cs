mrange/fsharpadvent2016

commit 000f530ea87f2114d41bb19b7108f6b5ea04a06b

Minor change to visibility

src/PHM/PHM.CS/PHM.cs
@@ -115,7 +115,7 @@ namespace PHM.CS
 
   static partial class PersistentHashMap
   {
-    public static PersistentHashMap<K, V> Empty<K, V> ()
+    internal static PersistentHashMap<K, V> Empty<K, V> ()
       where K : IEquatable<K>
     {
       return PersistentHashMap<K ,V>.EmptyNode;

commit 4c446673692d7fc8e8c28b98bd132bf13b13de76

Updated to use HMap rather than old version Vmap. Fixed compile error introduced by 000f530ea87f2114d41bb19b7108f6b5ea04a06b.

src/PHM/PHM.CS/PHM.cs
@@ -115,7 +115,7 @@ namespace PHM.CS
 
   static partial class PersistentHashMap
   {
-    internal static PersistentHashMap<K, V> Empty<K, V> ()
+    public static PersistentHashMap<K, V> Empty<K, V> ()
       where K : IEquatable<K>
     {
       return PersistentHashMap<K ,V>.EmptyNode;

src/PHM/Perf.PHM/Program.fs
@@ -324,11 +324,11 @@
     open Prime
 
     let createTestCases lookups inserts removals = 
-      let inline containsKey  k   m = Vmap.containsKey k   m
-      let inline length           m = Vmap.toSeq           m |> Seq.length
-      let inline isEmpty          m = Vmap.isEmpty         m
-      let inline set          k v m = Vmap.add         k v m
-      let inline unset        k   m = Vmap.remove      k   m
+      let inline containsKey  k   m = HMap.containsKey k   m
+      let inline length           m = HMap.toSeq           m |> Seq.length
+      let inline isEmpty          m = HMap.isEmpty         m
+      let inline set          k v m = HMap.add         k v m
+      let inline unset        k   m = HMap.remove      k   m
 
       let inline doInsert phm =
         let rec loop phm i =
@@ -357,7 +357,7 @@
             true
         loop 0
 
-      let empty     = Vmap.makeEmpty ()
+      let empty     = HMap.makeEmpty ()
 
       let inserted  = doInsert empty
 
@@ -505,10 +505,9 @@
 
     let testCases =
       [|
-        "Persistent Hash Map (C#)"      , CsPersistentHashMap.createTestCases
         "Persistent Hash Map (F#)"      , FsPersistentHashMap.createTestCases
         "FSharpx.Collections"           , FSharpx.createTestCases
-        "Prime.Vmap"                    , PrimeVmap.createTestCases
+        "Prime.HMap"                    , PrimeVmap.createTestCases
         "Imms.ImmMap"                   , ImmsImmMap.createTestCases
         "System.Collections.Immutable"  , SCI.createTestCases
         "Map (F#)"                      , FsMap.createTestCases

src/PHM/Test.PHM.CS/Prime.fs
@@ -188,55 +188,77 @@ module Operators =
     let inline (===) (x : 'a) (y : 'a) = similar x y
 
 
-/// A hash-key-value triple, implemented with a struct for efficiency.
-type internal Hkv<'k, 'v when 'k : comparison> =
-    struct
-        new (h, k, v) = { H = h; K = k; V = v }
-        val H : int
-        val K : 'k
-        val V : 'v
-        end
-
 [<AutoOpen>]
-module VmapModule =
+module HMapModule =
 
-    /// TODO: there's an F# issue where UseNullAsTrueValue does not work on unions with 4 or more cases
-    /// https://github.com/Microsoft/visualfsharp/issues/711 . Once resolved, should use it and be able
-    /// to make arrays with Array.zeroCreate alone without also copying over the empty array.
-    type [<NoComparison>] private Vnode<'k, 'v when 'k : comparison> =
+    /// A hash-key-value triple, implemented with a struct for efficiency.
+    type private Hkv<'k, 'v when 'k :> IEquatable<'k>> =
+        struct
+            new (h, k, v) = { H = h; K = k; V = v }
+            val H : int
+            val K : 'k
+            val V : 'v
+            end
+
+    /// TODO: P1: there's an F# issue where UseNullAsTrueValue does not work on unions with 4 or
+    /// more cases https://github.com/Microsoft/visualfsharp/issues/711 . Once resolved, should use
+    /// it and be able to make arrays with Array.zeroCreate alone without also copying over the
+    /// empty array.
+    type [<NoComparison>] private HNode<'k, 'v when 'k :> IEquatable<'k>> =
         | Nil
         | Singleton of Hkv<'k, 'v>
-        | Multiple of Vnode<'k, 'v> array
-        | Clash of Map<'k, 'v>
+        | Multiple of HNode<'k, 'v> array
+        | Bucket of Hkv<'k, 'v> array
 
     [<RequireQualifiedAccess>]
-    module private Vnode =
+    module private HNode =
 
         let inline failwithKeyNotFound (k : 'k) =
-            raise ^ KeyNotFoundException ^ "Could not find Vmap key '" + k.ToString () + "'."
+            raise ^ KeyNotFoundException ^ "Could not find HMap key '" + k.ToString () + "'."
 
         /// OPTIMIZATION: Array.Clone () is not used since it's been profiled to be slower
-        let inline cloneArray (arr : Vnode<'k, 'v> array) : Vnode<'k, 'v> array =
-            let arr' = Array.zeroCreate 32  // NOTE: there's an unecessary check against the size here, but that's the only inefficiency
-                                            // TODO: use Array.zeroCreateUnchecked if / when it becomes available
+        let inline private cloneArray (arr : HNode<'k, 'v> array) =
+            let arr' = Array.zeroCreate 32 : HNode<'k, 'v> array    // NOTE: there's an unecessary check against the size here, but that's the only inefficiency
+                                                                    // TODO: P1: use Array.zeroCreateUnchecked if / when it becomes available
             Array.Copy (arr, 0, arr', 0, 32) // param checks are inefficient, but hopefully there's at least a memcpy underneath...
             arr'
-
+    
         let inline private hashToIndex h dep =
             (h >>> (dep * 5)) &&& 0x1F
+
+        let private addToBucket (entry : Hkv<'k, 'v>) (bucket : Hkv<'k, 'v> array) =
+            let bucketLength = bucket.Length
+            let bucket2 = Array.zeroCreate (bucketLength + 1) : Hkv<'k, 'v> array
+            Array.Copy (bucket, 0, bucket2, 0, bucketLength)
+            bucket2.[bucketLength] <- entry
+            bucket2
+
+        let private removeFromBucket (k : 'k) (bucket : Hkv<'k, 'v> array) =
+            match Array.tryFindIndexBack (fun (entry2 : Hkv<'k, 'v>) -> entry2.K.Equals k) bucket with
+            | Some index ->
+                let bucket2 = Array.zeroCreate (bucket.Length - 1) : Hkv<'k, 'v> array
+                Array.Copy (bucket, 0, bucket2, 0, index)
+                Array.Copy (bucket, index + 1, bucket2, index, bucket2.Length - index)
+                bucket2
+            | None -> bucket
+
+        let private tryFindInBucket (k : 'k) (bucket : Hkv<'k, 'v> array) =
+            match Array.tryFindBack (fun (entry2 : Hkv<'k, 'v>) -> entry2.K.Equals k) bucket with
+            | Some hkv -> Some hkv.V
+            | None -> None
 
         let isEmpty node =
             match node with
             | Nil -> true
             | _ -> false
-
+    
         let rec fold folder state node =
             match node with
             | Nil -> state
             | Singleton hkv -> folder state hkv.K hkv.V
             | Multiple arr -> Array.fold (fold folder) state arr
-            | Clash clashMap -> Map.fold folder state clashMap
-
+            | Bucket bucket -> Array.fold (fun state (hkv : Hkv<_, _>) -> folder state hkv.K hkv.V) state bucket
+    
         /// NOTE: This function seems to profile as being very slow. I don't know if it's the seq / yields syntax or what.
         let rec toSeq node =
             seq {
@@ -244,23 +266,23 @@ module VmapModule =
                 | Nil -> yield! Seq.empty
                 | Singleton hkv -> yield (hkv.K, hkv.V)
                 | Multiple arr -> for n in arr do yield! toSeq n
-                | Clash clashMap -> yield! Map.toSeq clashMap }
-
+                | Bucket bucket -> yield! Array.map (fun (hkv : Hkv<_, _>) -> (hkv.K, hkv.V)) bucket }
+    
         /// OPTIMIZATION: Requires an empty array to use the source of new array clones in order to avoid Array.create.
-        let rec add (hkv : Hkv<'k, 'v>) (earr : Vnode<'k, 'v> array) (dep : int) (node : Vnode<'k, 'v>) : Vnode<'k, 'v> =
-
+        let rec add (hkv : Hkv<'k, 'v>) (earr : HNode<'k, 'v> array) (dep : int) (node : HNode<'k, 'v>) : HNode<'k, 'v> =
+    
             // lower than max depth, non-clashing
             if dep < 8 then
-
+    
                 // handle non-clash cases
                 match node with
                 | Nil ->
-
+    
                     // make singleton entry
                     Singleton hkv
-
+    
                 | Singleton hkv' ->
-
+                    
                     // if additional entry; convert Singleton to Multiple
                     let idx = hashToIndex hkv.H dep
                     let idx' = hashToIndex hkv'.H dep
@@ -269,11 +291,11 @@ module VmapModule =
                         arr.[idx] <- Singleton hkv
                         arr.[idx'] <- Singleton hkv'
                         Multiple arr
-
+    
                     // if replace entry; remain Singleton
-                    elif hkv.K == hkv'.K then
+                    elif hkv.K.Equals hkv'.K then
                         Singleton hkv
-
+    
                     // if add entry with same idx; add both in new node
                     else
                         let dep' = dep + 1
@@ -282,157 +304,157 @@ module VmapModule =
                         let arr = cloneArray earr
                         arr.[idx] <- node'
                         Multiple arr
-
+    
                 | Multiple arr ->
-
+    
                     // add entry with recursion
                     let idx = hashToIndex hkv.H dep
                     let entry = arr.[idx]
                     let arr = cloneArray arr
                     arr.[idx] <- add hkv earr (dep + 1) entry
                     Multiple arr
-
-                | Clash _ ->
-
+    
+                | Bucket _ ->
+    
                     // logically should never hit here
                     failwithumf ()
-
+    
             // clashing
             else
-
+                
                 // handle clash cases
                 match node with
-                | Nil -> Clash ^ singleton hkv.K hkv.V
-                | Singleton hkv' -> Clash ^ Map.add hkv.K hkv.V ^ singleton hkv'.K hkv'.V
+                | Nil -> Bucket (Array.singleton hkv)
+                | Singleton hkv' -> Bucket [|hkv'; hkv|]
                 | Multiple _ -> failwithumf () // should never hit here
-                | Clash clashMap -> Clash ^ Map.add hkv.K hkv.V clashMap
-
-        let rec remove (h : int) (k : 'k) (dep : int) (node : Vnode<'k, 'v>) : Vnode<'k, 'v> =
+                | Bucket bucket -> Bucket (addToBucket hkv bucket)
+    
+        let rec remove (h : int) (k : 'k) (dep : int) (node : HNode<'k, 'v>) : HNode<'k, 'v> =
             match node with
             | Nil -> node
-            | Singleton hkv -> if hkv.K == k then Nil else node
+            | Singleton hkv -> if hkv.K.Equals k then Nil else node
             | Multiple arr ->
                 let idx = hashToIndex h dep
                 let entry = arr.[idx]
                 let arr = cloneArray arr
                 arr.[idx] <- remove h k (dep + 1) entry
                 if Array.forall isEmpty arr then Nil else Multiple arr // does not collapse Multiple to Singleton, tho could?
-            | Clash clashMap ->
-                let clashMap = Map.remove k clashMap
-                if Map.isEmpty clashMap then Nil else Clash clashMap
-
-        let rec tryFind (h : int) (k : 'k) (dep : int) (node : Vnode<'k, 'v>) : 'v option =
+            | Bucket bucket ->
+                let bucket = removeFromBucket k bucket
+                if Array.isEmpty bucket then Nil else Bucket bucket
+    
+        let rec tryFind (h : int) (k : 'k) (dep : int) (node : HNode<'k, 'v>) : 'v option =
             match node with
             | Nil -> None
-            | Singleton hkv -> if hkv.K == k then Some hkv.V else None
+            | Singleton hkv -> if hkv.K.Equals k then Some hkv.V else None
             | Multiple arr -> let idx = hashToIndex h dep in tryFind h k (dep + 1) arr.[idx]
-            | Clash clashMap -> Map.tryFind k clashMap
-
-        let rec find (h : int) (k : 'k) (dep : int) (node : Vnode<'k, 'v>) : 'v =
+            | Bucket bucket -> tryFindInBucket k bucket
+    
+        let rec find (h : int) (k : 'k) (dep : int) (node : HNode<'k, 'v>) : 'v =
             match node with
             | Nil -> failwithKeyNotFound k
-            | Singleton hkv -> if hkv.K == k then hkv.V else failwithKeyNotFound k
+            | Singleton hkv -> if hkv.K.Equals k then hkv.V else failwithKeyNotFound k
             | Multiple arr -> let idx = hashToIndex h dep in find h k (dep + 1) arr.[idx]
-            | Clash clashMap -> Map.find k clashMap
-
+            | Bucket bucket -> Option.get (tryFindInBucket k bucket)
+    
         let empty =
             Nil
 
-    /// A very fast persistent hash map.
+    /// A fast persistent hash map.
     /// Works in effectively constant-time for look-ups and updates.
-    type [<NoComparison>] Vmap<'k, 'v when 'k : comparison> =
+    type [<NoComparison>] HMap<'k, 'v when 'k :> IEquatable<'k>> =
         private
-            { Node : Vnode<'k, 'v>
-              EmptyArray : Vnode<'k, 'v> array }
-
+            { Node : HNode<'k, 'v>
+              EmptyArray : HNode<'k, 'v> array }
+    
         interface IEnumerable<'k * 'v> with
-            member this.GetEnumerator () = (Vnode.toSeq this.Node).GetEnumerator ()
-
+            member this.GetEnumerator () = (HNode.toSeq this.Node).GetEnumerator ()
+    
         interface IEnumerable with
-            member this.GetEnumerator () = (Vnode.toSeq this.Node).GetEnumerator () :> IEnumerator
+            member this.GetEnumerator () = (HNode.toSeq this.Node).GetEnumerator () :> IEnumerator
 
     [<RequireQualifiedAccess>]
-    module Vmap =
-
-        /// Create an empty Vmap.
+    module HMap =
+    
+        /// Create an empty HMap.
         let makeEmpty () =
-            { Node = Vnode.empty
-              EmptyArray = Array.create 32 Vnode.empty }
-
-        /// Check that a Vmap is empty.
+            { Node = HNode.empty
+              EmptyArray = Array.create 32 HNode.empty }
+    
+        /// Check that a HMap is empty.
         let isEmpty map =
-            Vnode.isEmpty map.Node
-
-        /// Check that a Vmap is empty.
+            HNode.isEmpty map.Node
+    
+        /// Check that a HMap is empty.
         let notEmpty map =
-            not ^ Vnode.isEmpty map.Node
-
-        /// Add a value with the key to a Vmap.
+            not ^ HNode.isEmpty map.Node
+    
+        /// Add a value with the key to a HMap.
         let add (key : 'k) (value : 'v) map =
             let hkv = Hkv (key.GetHashCode (), key, value)
-            let node = Vnode.add hkv map.EmptyArray 0 map.Node
+            let node = HNode.add hkv map.EmptyArray 0 map.Node
             { map with Node = node }
-
-        /// Remove a value with the given key from a Vmap.
+    
+        /// Remove a value with the given key from a HMap.
         let remove (key : 'k) map =
             let h = key.GetHashCode ()
-            { map with Node = Vnode.remove h key 0 map.Node }
-
-        /// Add all the given entries to a Vmap.
+            { map with Node = HNode.remove h key 0 map.Node }
+    
+        /// Add all the given entries to a HMap.
         let addMany entries map =
             Seq.fold (fun map (key : 'k, value : 'v) -> add key value map) map entries
-
-        /// Remove all values with the given keys from a Vmap.
+    
+        /// Remove all values with the given keys from a HMap.
         let removeMany keys map =
             Seq.fold (fun map (key : 'k) -> remove key map) map keys
-
-        /// Try to find a value with the given key in a Vmap.
+    
+        /// Try to find a value with the given key in a HMap.
         /// Constant-time complexity with approx. 1/3 speed of Dictionary.TryGetValue.
         let tryFind (key : 'k) map : 'v option =
             let h = key.GetHashCode ()
-            Vnode.tryFind h key 0 map.Node
-
-        /// Find a value with the given key in a Vmap.
+            HNode.tryFind h key 0 map.Node
+            
+        /// Find a value with the given key in a HMap.
         /// Constant-time complexity with approx. 1/3 speed of Dictionary.GetValue.
         let find (key : 'k) map : 'v =
             let h = key.GetHashCode ()
-            Vnode.find h key 0 map.Node
-
-        /// Check that a Vmap contains a value with the given key.
+            HNode.find h key 0 map.Node
+    
+        /// Check that a HMap contains a value with the given key.
         let containsKey key map =
             match tryFind key map with
             | Some _ -> true
             | None -> false
-
-        /// Combine the contents of two Vmaps, taking an item from the second map in the case of a key conflict.
+            
+        /// Combine the contents of two HMaps, taking an item from the second map in the case of a key conflict.
         let concat map map2 =
             Seq.fold (flip ^ uncurry add) map map2
-
-        /// Fold over a Vmap.
-        let fold folder state (map : Vmap<'k, 'v>) =
-            Vnode.fold folder state map.Node
-
-        /// Map over a Vmap.
-        let map mapper (map : Vmap<'k, 'v>) =
+    
+        /// Fold over a HMap.
+        let fold folder state (map : HMap<'k, 'v>) =
+            HNode.fold folder state map.Node
+    
+        /// Map over a HMap.
+        let map mapper (map : HMap<'k, 'v>) =
             fold
                 (fun state key value -> add key (mapper value) state)
                 (makeEmpty ())
                 map
-
-        /// Filter a Vmap.
-        let filter pred (map : Vmap<'k, 'v>) =
+    
+        /// Filter a HMap.
+        let filter pred (map : HMap<'k, 'v>) =
             fold
                 (fun state key value -> if pred key value then add key value state else state)
                 (makeEmpty ())
                 map
-
-        /// Convert a Vmap to a sequence of pairs of keys and values.
+    
+        /// Convert a HMap to a sequence of pairs of keys and values.
         /// NOTE: This function seems to profile as being very slow. I don't know if it's the seq / yields syntax or what.
         /// Don't use it unless you need its laziness or if performance won't be affected significantly.
-        let toSeq (map : Vmap<'k, 'v>) =
+        let toSeq (map : HMap<'k, 'v>) =
             map :> IEnumerable<'k * 'v>
-
-        /// Convert a sequence of keys and values to a Vmap.
+    
+        /// Convert a sequence of keys and values to a HMap.
         let ofSeq pairs =
             Seq.fold
                 (fun map (key, value) -> add key value map)
@@ -441,4 +463,4 @@ module VmapModule =
 
 /// A very fast persistent hash map.
 /// Works in effectively constant-time for look-ups and updates.
-type Vmap<'k, 'v when 'k : comparison> = VmapModule.Vmap<'k, 'v>
+type HMap<'k, 'v when 'k :> IEquatable<'k>> = HMapModule.HMap<'k, 'v>
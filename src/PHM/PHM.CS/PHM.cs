4c446673692d7fc8e8c28b98bd132bf13b13de76
Bryan <bryan@jet.com>
2017-02-16 12:11:18 -0500

    Updated to use HMap rather than old version Vmap. Fixed compile error introduced by 000f530ea87f2114d41bb19b7108f6b5ea04a06b.

--- src/PHM/PHM.CS/PHM.cs
+++ src/PHM/PHM.CS/PHM.cs
@@ -115,7 +115,7 @@ namespace PHM.CS
 
   static partial class PersistentHashMap
   {
-    internal static PersistentHashMap<K, V> Empty<K, V> ()
+    public static PersistentHashMap<K, V> Empty<K, V> ()
       where K : IEquatable<K>
     {
       return PersistentHashMap<K ,V>.EmptyNode;
